dca01617bbc6bbf145c0d63a2c8dfd2f72203725
Dezteeney <deze2010@yahoo.com>
2022-09-29 14:04:05 -0600

    first commit

---
+++ src/Example1.java
@@ -0,0 +1,19 @@
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello World");
+        
+        int num1 = 10;
+        char firstCharOfYourName = 'k';
+        float floatValue = (float)56.2;
+        
+        System.out.println(num1);
+        System.out.println(firstCharOfYourName);
+        System.out.println(floatValue);
+        
+        		
+	}
+           
+}
